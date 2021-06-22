29a8d323fa87d97b0ef4933791f1df133c0d36c0
Juan Fredes <juan.fredes@alumnos.frm.utn.edu.ar>
2021-06-22 02:58:37 -0300

    Modificacion de las Clases Abuelo y Voluntario

--- BACK_END/abuelo.py
+++ BACK_END/abuelo.py
@@ -1,23 +1,42 @@
 from DB.conexion import DAO
-
+from BACK_END.voluntario import Voluntario
+from UI import funciones 
 
 class Abuelo:
-    id = 0
-    nombre = ""
-    apellido = ""
-    direccion = ""
-    sexo = ""
+    # usuario = ""
+    # contra = ""
+    # nombre = ""
+    # apellido = ""
+    # direccion = ""
+    # sexo = ""
 
-    def __init__(self):
-        pass
+    def __init__(self, usuario, contra, nombre, apellido, tel, celular, direccion, sexo):
+        self.usuario = usuario
+        self.contra = contra
+        self.nombre = nombre
+        self.apellido = apellido
+        self.tel = tel
+        self.celular = celular
+        self.direccion = direccion
+        self.sexo = sexo
+        self.ayudante = None
 
+    def iniciar_sesion(usuario):
+        try:
+            dao = DAO()
+            sql = f'select contra, nombre, apellido, tel, celular, direccion, sexo from abuelo where usuario=\'{usuario}\';'
+            lista = dao.recuperarRegistro(sql)
+            return Abuelo(usuario,lista[0],lista[1],lista[2],lista[3],lista[4], lista[5], lista[6])
+        except Exception as e:
+            print(e)
+    
     #  UI PARA INGRESAR NUEVOS ABUELOS
-    def guardar_nuevo(self):
+    def resgistrarse(self):
         # noinspection PyBroadException
         try:
             dao = DAO()
-            sql = "INSERT INTO abuelo (nombre, apellido, direccion, sexo) VALUES ('{0}','{1}','{2}','{3}');"
-            sql = sql.format(self.nombre, self.apellido, self.direccion, self.sexo)
+            sql = "INSERT INTO abuelo (usuario, contra, nombre, apellido, tel, celular, direccion, sexo) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}');"
+            sql = sql.format(self.usuario, self.contra, self.nombre, self.apellido, self.tel, self.celular, self.direccion, self.sexo)
             dao.insertarOActualizar(sql)
         except Exception as e:
             print(e)
@@ -26,68 +45,172 @@ class Abuelo:
         # noinspection PyBroadException
         try:
             dao = DAO()
-            sql = "UPDATE abuelo SET nombre = '{0}', apellido = '{1}', direccion = '{2}', sexo = '{3}' WHERE id = {4};"
-            sql = sql.format(self.nombre, self.apellido, self.direccion, self.sexo, self.id)
+            sql = "UPDATE abuelo SET contra = '{0}', nombre = '{1}', apellido = '{2}', tel = '{3}', celular = '{4}', direccion = '{5}', sexo = '{6}' WHERE usuario = '{7}';"
+            sql = sql.format(self.contra, self.nombre, self.apellido, self.tel, self.celular, self.direccion, self.sexo, self.usuario)
+            dao.insertarOActualizar(sql)
+        except Exception as e:
+            print(e)
+
+    def mostrarDatos(self):
+        print('Nombre:',self.nombre)
+        print('Apellido:',self.apellido)
+        print('Tel.:',self.tel)
+        print('Celular:',self.celular)
+        print('Dirección:',self.direccion)
+        print('Sexo:',self.sexo)
+    
+    def eliminarme(self):
+        try:
+            dao = DAO()
+            sql = "DELETE FROM abuelo WHERE usuario = '{0}'"
+            sql = sql.format(self.usuario)
+            dao.insertarOActualizar(sql)
+        except Exception as e:
+            print(e)
+
+    def lista_volutariosDisponibles(self):
+        miLatLong = funciones.latitudLongitud(self.direccion)
+        l = Voluntario.lista_voluntariosDisponibles()
+        listaVolDisp = []
+        for usr in l:
+            if (funciones.haversine(miLatLong, funciones.latitudLongitud(usr[1]))) <= 1:
+                listaVolDisp = list(listaVolDisp) + [(usr)]
+        return listaVolDisp
+
+    def pedirAyuda(self,usr_voluntario):
+        try:
+            dao = DAO()
+            sql = f"UPDATE voluntario SET peticionAyuda = '{self.usuario}' WHERE usuario = '{usr_voluntario}';"
+            dao.insertarOActualizar(sql)
+        except Exception as e:
+            print(e)
+
+    def mostrarDatosVoluntario(self, ayudante):
+        try:
+            dao = DAO()
+            sql = f"SELECT nombre, apellido, celular, tel FROM voluntario WHERE usuario = '{ayudante}'"
+            datos = dao.recuperarRegistro(sql)
+            print(
+                'Datos del voluntario para que pueda comunicarse:\n'
+                'Nombre:',datos[0],'\n'
+                'Apellido:',datos[1],'\n'
+                'Celular:',datos[2],'\n'
+                'Tel.:',datos[3],
+                    )
+        except Exception as e:
+            print(e)
+
+    def actualizarPedido(self):
+        try:
+            dao = DAO()
+            sql = f"SELECT ayudante FROM abuelo WHERE usuario = '{self.usuario}';"
+            ayudante = dao.recuperarRegistro(sql)
+            if ayudante[0] != None and ayudante[0] != '0':
+                self.mostrarDatosVoluntario(ayudante[0])
+                return 'aceptado'
+            elif ayudante[0] == '0':
+                print('El voluntario a rechazado su pedido')
+                return 'rechazado'
+            else: return 'sigue'
+        except Exception as e:
+            print(e)
+    
+    def ayudaCompletadaORechazada(self):
+        try:
+            dato = 'NULL'
+            dao = DAO()
+            sql = "UPDATE abuelo SET ayudante = {0} WHERE usuario = '{1}';"
+            sql = sql.format(str(dato),self.usuario)
             dao.insertarOActualizar(sql)
         except Exception as e:
             print(e)
 
     @staticmethod
-    def listar_abuelos():
-        # noinspection PyBroadException
+    def verificar_login(usuario, contra):
         try:
             dao = DAO()
-            resultados = dao.recuperarLista("SELECT id, nombre, apellido, direccion, sexo FROM abuelo ORDER BY id ASC")
-
-            if len(resultados) > 0:
-                lista_abuelos = list()
-                for fila in resultados:
-                    un_abuelo = Abuelo()
-                    un_abuelo.id = fila[0]
-                    un_abuelo.nombre = fila[1]
-                    un_abuelo.apellido = fila[2]
-                    un_abuelo.direccion = fila[3]
-                    un_abuelo.sexo = fila[4]
-                    lista_abuelos.append(un_abuelo)
-
-                return lista_abuelos
+            sql = f"SELECT contra FROM abuelo WHERE usuario='{usuario}' AND contra='{contra}';"
+            if dao.recuperarRegistro(sql):
+                return True
             else:
-                return None
-
+                return False
         except Exception as e:
             print(e)
-
+            
     @staticmethod
-    def cargar_abuelo(idabuelo):
-        # noinspection PyBroadException
+    def verificar_usuarioDisponible(usuario):
         try:
             dao = DAO()
-            sql = "SELECT * FROM abuelo WHERE id = {0}"
-            sql = sql.format(idabuelo)
-            resultado = dao.recuperarRegistro(sql)
-
-            if resultado is not None:
-                un_abuelo = Abuelo()
-                un_abuelo.id = resultado[0]
-                un_abuelo.nombre = resultado[1]
-                un_abuelo.apellido = resultado[2]
-                un_abuelo.direccion = resultado[3]
-                un_abuelo.sexo = resultado[4]
-                return un_abuelo
-            else:
-                print("No se encuentra el abuelo con el ID ingresado")
-                return None
-
+            sql = "SELECT usuario FROM abuelo;"
+            lista_usr = dao.recuperarRegistro(sql)
+            verif = True
+            if lista_usr != None:
+                for usr in lista_usr:
+                    if usr == usuario:
+                        verif = False
+                        return verif
+            return verif
         except Exception as e:
             print(e)
 
-    @staticmethod
-    def eliminar_abuelo(idabuelo):
-        # noinspection PyBroadException
-        try:
-            dao = DAO()
-            sql = "DELETE FROM abuelo WHERE id = {0}"
-            sql = sql.format(idabuelo)
-            dao.insertarOActualizar(sql)
-        except Exception as e:
-            print(e)
+    # @staticmethod
+    # def listar_abuelos():
+    #     # noinspection PyBroadException
+    #     try:
+    #         dao = DAO()
+    #         resultados = dao.recuperarLista("SELECT id, nombre, apellido, direccion, sexo FROM abuelo ORDER BY id ASC")
+
+    #         if len(resultados) > 0:
+    #             lista_abuelos = list()
+    #             for fila in resultados:
+    #                 un_abuelo = Abuelo()
+    #                 un_abuelo.id = fila[0]
+    #                 un_abuelo.nombre = fila[1]
+    #                 un_abuelo.apellido = fila[2]
+    #                 un_abuelo.direccion = fila[3]
+    #                 un_abuelo.sexo = fila[4]
+    #                 lista_abuelos.append(un_abuelo)
+
+    #             return lista_abuelos
+    #         else:
+    #             return None
+
+    #     except Exception as e:
+    #         print(e)
+
+    # @staticmethod
+    # def cargar_abuelo(idabuelo):
+    #     # noinspection PyBroadException
+    #     try:
+    #         dao = DAO()
+    #         sql = "SELECT * FROM abuelo WHERE id = {0}"
+    #         sql = sql.format(idabuelo)
+    #         resultado = dao.recuperarRegistro(sql)
+
+    #         if resultado is not None:
+    #             un_abuelo = Abuelo()
+    #             un_abuelo.id = resultado[0]
+    #             un_abuelo.nombre = resultado[1]
+    #             un_abuelo.apellido = resultado[2]
+    #             un_abuelo.direccion = resultado[3]
+    #             un_abuelo.sexo = resultado[4]
+    #             return un_abuelo
+    #         else:
+    #             print("No se encuentra el abuelo con el ID ingresado")
+    #             return None
+
+    #     except Exception as e:
+    #         print(e)
+
+    # @staticmethod
+    # def eliminar_abuelo(idabuelo):
+    #     # noinspection PyBroadException
+    #     try:
+    #         dao = DAO()
+    #         sql = "DELETE FROM abuelo WHERE id = {0}"
+    #         sql = sql.format(idabuelo)
+    #         dao.insertarOActualizar(sql)
+    #     except Exception as e:
+    #         print(e)
+    # def imprimir(self):
+    #     print(self.nombre)
--- BACK_END/voluntario.py
+++ BACK_END/voluntario.py
@@ -2,91 +2,233 @@ from DB.conexion import DAO
 
 
 class Voluntario:
-    id = 0
-    nombre = ""
-    apellido = ""
-    direccion = ""
-    sexo = ""
+    # id = 0
+    # nombre = ""
+    # apellido = ""
+    # direccion = ""
+    # sexo = ""
 
-    def __init__(self):
-        pass
+    def __init__(self, usuario, contra, nombre, apellido, tel, celular, direccion, sexo):
+        self.usuario = usuario
+        self.contra = contra
+        self.nombre = nombre
+        self.apellido = apellido
+        self.tel = tel
+        self.celular = celular
+        self.direccion = direccion
+        self.sexo = sexo
+        self.disponibilidad = 0  
+        self.peticionAyuda = None
 
-    def guardar_nuevo(self):
+    def iniciar_sesion(usuario):
+        try:
+            dao = DAO()
+            sql = f'select contra, nombre, apellido, tel, celular, direccion, sexo, disponibilidad from voluntario where usuario=\'{usuario}\';'
+            lista = dao.recuperarRegistro(sql)
+            return Voluntario(usuario,lista[0],lista[1],lista[2],lista[3],lista[4], lista[5], lista[6])
+        except Exception as e:
+            print(e)     
+
+    def resgistrarse(self):
         # noinspection PyBroadException
         try:
             dao = DAO()
-            sql = "INSERT INTO voluntario (nombre, apellido, direccion, sexo) VALUES ('{0}','{1}','{2}','{3}');"
-            sql = sql.format(self.nombre, self.apellido, self.direccion, self.sexo)
+            sql = "INSERT INTO voluntario (usuario, contra, nombre, apellido, tel, celular, direccion, sexo, disponibilidad) VALUES ('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','0');"
+            sql = sql.format(self.usuario, self.contra, self.nombre, self.apellido, self.tel, self.celular, self.direccion, self.sexo)
             dao.insertarOActualizar(sql)
         except Exception as e:
-            print(e)
+            print(e) 
 
     def guardar_cambios(self):
         # noinspection PyBroadException
         try:
             dao = DAO()
-            sql = "UPDATE voluntario SET nombre = '{0}', apellido = '{1}', direccion = '{2}', sexo = '{3}' WHERE id = {4};"
-            sql = sql.format(self.nombre, self.apellido, self.direccion, self.sexo, self.id)
+            sql = "UPDATE voluntario SET contra = '{0}', nombre = '{1}', apellido = '{2}', tel = '{3}', celular = '{4}', direccion = '{5}', sexo = '{6}' WHERE usuario = '{7}';"
+            sql = sql.format(self.contra, self.nombre, self.apellido, self.tel, self.celular, self.direccion, self.sexo, self.usuario)
+            dao.insertarOActualizar(sql)
+        except Exception as e:
+            print(e)
+
+    def cambiarDisponibilidad(self):
+        # noinspection PyBroadException
+        try:
+            dao = DAO()
+            disp = dao.recuperarRegistro(f"select disponibilidad from voluntario where usuario = '{self.usuario}';")
+            if disp[0] == 0:
+                sql = f"UPDATE voluntario SET disponibilidad = 1 WHERE usuario = '{self.usuario}';"
+            else:
+                sql = f"UPDATE voluntario SET disponibilidad = 0 WHERE usuario = '{self.usuario}';"
+            dao.insertarOActualizar(sql)
+        except Exception as e:
+            print(e)
+
+    def mostrarDatos(self):
+        print('Nombre:',self.nombre)
+        print('Apellido:',self.apellido)
+        print('Tel.:',self.tel)
+        print('Celular:',self.celular)
+        print('Dirección:',self.direccion)
+        print('Sexo:',self.sexo)
+
+    def mostrarDisponibilidad(self):
+        try:
+            dao = DAO()
+            disp = dao.recuperarRegistro(f"select disponibilidad from voluntario where usuario = '{self.usuario}';")
+            if disp[0] == 0:
+                return 'No disponible' 
+            else:
+                return 'Disponible'
+        except Exception as e:
+            print(e)
+
+    def eliminarme(self):
+        try:
+            dao = DAO()
+            sql = "DELETE FROM voluntario WHERE usuario = '{0}';"
+            sql = sql.format(self.usuario)
+            dao.insertarOActualizar(sql)
+        except Exception as e:
+            print(e)
+
+    def mostrarDatosAbuelo(self, abuelo):
+        try:
+            dao = DAO()
+            sql = f"SELECT nombre, apellido, celular, tel FROM abuelo WHERE usuario = '{abuelo}';"
+            datos = dao.recuperarRegistro(sql)
+            print(
+                'Datos del abuelo para que pueda comunicarse:\n'
+                'Nombre:',datos[0],'\n'
+                'Apellido:',datos[1],'\n'
+                'Celular:',datos[2],'\n'
+                'Tel.:',datos[3],
+                    )
+        except Exception as e:
+            print(e)
+
+    def actualizarNotificacion(self):
+        try:
+            dao = DAO()
+            resp = ''
+            sql = f"SELECT peticionAyuda FROM voluntario WHERE usuario = '{self.usuario}';"
+            peticion = dao.recuperarRegistro(sql)
+            if peticion[0] != None:
+                print(f'El usuario {peticion[0]} solicita su ayuda')
+                print('Opcion 1:\tAceptar\nOpcion 2:\tRechazar')
+                resp = input()
+                if resp == '1':
+                    self.mostrarDatosAbuelo(peticion[0])
+                    dao.insertarOActualizar(f'UPDATE abuelo SET ayudante = \'{self.usuario}\' WHERE usuario = \'{peticion[0]}\';')
+                elif resp == '2':
+                    dao.insertarOActualizar(f'UPDATE abuelo SET ayudante = \'0\' WHERE usuario = \'{peticion[0]}\';')
+            else:
+                print('No tiene ninguna petición de ayuda.')
+        except Exception as e:
+            print(e)
+    
+    def ayudaCompletadaORechazada(self):
+        try:
+            dato = 'NULL'
+            dao = DAO()
+            sql = "UPDATE voluntario SET peticionAyuda = {0} WHERE usuario = '{1}';"
+            sql = sql.format(str(dato),self.usuario)
             dao.insertarOActualizar(sql)
         except Exception as e:
             print(e)
 
     @staticmethod
-    def listar_voluntarios():
-        # noinspection PyBroadException
+    def verificar_login(usuario, contra):
         try:
             dao = DAO()
-            resultados = dao.recuperarLista("SELECT id, nombre, apellido, direccion, sexo FROM voluntario ORDER BY id ASC")
-
-            if len(resultados) > 0:
-                lista_voluntarios = list()
-                for fila in resultados:
-                    un_voluntario = Voluntario()
-                    un_voluntario.id = fila[0]
-                    un_voluntario.nombre = fila[1]
-                    un_voluntario.apellido = fila[2]
-                    un_voluntario.direccion = fila[3]
-                    un_voluntario.sexo = fila[4]
-                    lista_voluntarios.append(un_voluntario)
-
-                return lista_voluntarios
+            sql = f"SELECT contra FROM voluntario WHERE usuario='{usuario}' AND contra='{contra}';"
+            if dao.recuperarRegistro(sql):
+                return True
             else:
-                return None
-
+                return False
         except Exception as e:
             print(e)
 
     @staticmethod
-    def cargar_voluntario(idvoluntario):
-        # noinspection PyBroadException
+    def verificar_usuarioDisponible(usuario):
         try:
             dao = DAO()
-            sql = "SELECT * FROM voluntario WHERE id = {0}"
-            sql = sql.format(idvoluntario)
-            resultado = dao.recuperarRegistro(sql)
-
-            if resultado is not None:
-                un_voluntario = Voluntario()
-                un_voluntario.id = resultado[0]
-                un_voluntario.nombre = resultado[1]
-                un_voluntario.apellido = resultado[2]
-                un_voluntario.direccion = resultado[3]
-                un_voluntario.sexo = resultado[4]
-                return un_voluntario
-            else:
-                print("No se encuentra el voluntario con el ID ingresado")
-                return None
+            sql = "SELECT usuario FROM voluntario;"
+            lista_usr = dao.recuperarRegistro(sql)
+            verif = True
+            if lista_usr != None:
+                for usr in lista_usr:
+                    if usr == usuario:
+                        verif = False
+                        return verif
+            return verif
         except Exception as e:
             print(e)
 
     @staticmethod
-    def eliminar_voluntario(idvoluntario):
-        # noinspection PyBroadException
+    def lista_voluntariosDisponibles():
         try:
             dao = DAO()
-            sql = "DELETE FROM voluntario WHERE id={0}"
-            sql = sql.format(idvoluntario)
-            dao.insertarOActualizar(sql)
-
+            sql = "SELECT usuario, direccion FROM voluntario WHERE disponibilidad = 1;"
+            lista = dao.recuperarLista(sql)
+            return lista
         except Exception as e:
             print(e)
+
+    # @staticmethod
+    # def listar_voluntarios():
+    #     # noinspection PyBroadException
+    #     try:
+    #         dao = DAO()
+    #         resultados = dao.recuperarLista("SELECT id, nombre, apellido, direccion, sexo FROM voluntario ORDER BY id ASC")
+
+    #         if len(resultados) > 0:
+    #             lista_voluntarios = list()
+    #             for fila in resultados:
+    #                 un_voluntario = Voluntario()
+    #                 un_voluntario.id = fila[0]
+    #                 un_voluntario.nombre = fila[1]
+    #                 un_voluntario.apellido = fila[2]
+    #                 un_voluntario.direccion = fila[3]
+    #                 un_voluntario.sexo = fila[4]
+    #                 lista_voluntarios.append(un_voluntario)
+
+    #             return lista_voluntarios
+    #         else:
+    #             return None
+
+    #     except Exception as e:
+    #         print(e)
+
+    # @staticmethod
+    # def cargar_voluntario(idvoluntario):
+    #     # noinspection PyBroadException
+    #     try:
+    #         dao = DAO()
+    #         sql = "SELECT * FROM voluntario WHERE id = {0}"
+    #         sql = sql.format(idvoluntario)
+    #         resultado = dao.recuperarRegistro(sql)
+
+    #         if resultado is not None:
+    #             un_voluntario = Voluntario()
+    #             un_voluntario.id = resultado[0]
+    #             un_voluntario.nombre = resultado[1]
+    #             un_voluntario.apellido = resultado[2]
+    #             un_voluntario.direccion = resultado[3]
+    #             un_voluntario.sexo = resultado[4]
+    #             return un_voluntario
+    #         else:
+    #             print("No se encuentra el voluntario con el ID ingresado")
+    #             return None
+    #     except Exception as e:
+    #         print(e)
+
+    # @staticmethod
+    # def eliminar_voluntario(idvoluntario):
+    #     # noinspection PyBroadException
+    #     try:
+    #         dao = DAO()
+    #         sql = "DELETE FROM voluntario WHERE id={0}"
+    #         sql = sql.format(idvoluntario)
+    #         dao.insertarOActualizar(sql)
+
+    #     except Exception as e:
+    #         print(e)
